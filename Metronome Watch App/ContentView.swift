4096d0a3d0c8827d3503da17b189416cb8d96531
Martin Hackl <martin.hackl@bnet.at>
2024-04-03 16:01:04 +0200

    Adding visual toggle button functionality, changing its color, boolean state and text when its pressed.

--- Metronome Watch App/ContentView.swift
+++ Metronome Watch App/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isOn = false
+    @State private var BPM = 90
+    @State private var startstop = "Start"
+    @State private var buttoncolor: Color = .green
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("BPM:")
+                .font(.headline)
+            Stepper(value: $BPM, in: 30...200) {
+                Text("\(BPM)")
+            }
+            
+            
+            
+            Button(action: {
+                if (isOn == false) {
+                    isOn = true;
+                    startstop = "Stop"
+                    buttoncolor = .red;
+                    print(isOn);
+                    print(BPM);
+                }
+                else{
+                    isOn = false;
+                    startstop = "Start"
+                    buttoncolor = .green;
+                    print(isOn);
+                }
+            }, label: {
+                Text("\(startstop)")
+            })
+            .buttonStyle(.borderedProminent)
+            .tint(buttoncolor)
+
+
         }
         .padding()
+        
     }
 }
 
