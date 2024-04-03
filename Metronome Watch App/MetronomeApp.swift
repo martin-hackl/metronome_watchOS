4690a063e5d3b6fb1e3d8691c107e706900f4f89
Martin Hackl <martin.hackl@bnet.at>
2024-04-03 09:31:00 +0200

    Initial Commit

---
+++ Metronome Watch App/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome Watch App
+//
+//  Created by Martin Hackl on 03.04.24.
+//
+
+import SwiftUI
+
+@main
+struct Metronome_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
